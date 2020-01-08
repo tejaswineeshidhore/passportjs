224123cb5a13a6893565b6dbf140a94c757a6993
Tejaswinee <tejaswineeshidhore@gmail.com>
2020-01-08 21:43:31 +0530

    first-commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb://tejaswinee:tO2Qz3fNuUvcKMlx@cluster0-shard-00-00-undnt.mongodb.net:27017,cluster0-shard-00-01-undnt.mongodb.net:27017,cluster0-shard-00-02-undnt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+
